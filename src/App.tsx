c5c928d6801eda9a437db461fcf610065045dacc
zhaochao <zhaochao0801@sina.com>
2024-04-16 20:45:49 +0800

    antd的使用.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Comp1 from "@/components/Comp1"
 import Comp2 from '@/components/Comp2'
+import { Button } from "antd"
+import { UpCircleOutlined } from "@ant-design/icons"
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
       <div>顶级组件</div>
       <Comp1></Comp1>
       <Comp2></Comp2>
+      <Button type="primary">antd按钮</Button>
+      <UpCircleOutlined style={{fontSize: '40px', color: 'blue'}}/>
     </>
   )
 }
--- src/components/Comp2/index.tsx
+++ src/components/Comp2/index.tsx
@@ -1,11 +1,10 @@
 
-function Comp2() {
-
+const Comp2 = () => {
     return (
         <div className="box">
-            <p>这是Comp2里面的内容</p>
+            <p>这是Comp2里面的内容，这个是箭头函数式组件。</p>
         </div>
-    )
+    ) 
 }
 
 export default Comp2
