dad3e4262b87a5a6874c0239060e1785f3672e6b
zhaochao <zhaochao0801@sina.com>
2024-04-17 06:15:51 +0800

    旧版本路由的跳转和重定向.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom"
 
 function App() {
 
   return (
     <div className="App">
+      <Link to="/home">Home</Link> |
+      <Link to="/about">About</Link>
       <Outlet></Outlet>
     </div>
   )
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,14 @@
 import App from "@/App";
 import Home from "@/views/Home";
 import About from "@/views/About";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 const baseRouter = () => (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<App />}>
+                {/* 重定向，当访问/时，重定向到/home */}
+                <Route path="/" element={<Navigate to="/home" />}></Route>
                 <Route path="/home" element={<Home />}></Route>
                 <Route path="/about" element={<About />}></Route>
             </Route>
