1bd792dc512e1f3be3f39db7205343a0442260e7
zhaochao <zhaochao0801@sina.com>
2024-05-21 07:04:10 +0800

    登录页面输入框

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,5 +1,7 @@
 import initLoginBg from "./init"
 import styles from './login.module.scss'
+import './login.less'
+import { Space, Input, Button } from 'antd'
 import { useEffect } from "react"
 
 const Login = () => {
@@ -17,12 +19,19 @@ const Login = () => {
             {/* 存放背景 */}
             <canvas id="canvas" style={{ display: 'block' }}></canvas>
             {/* 登录盒子 */}
-            <div className={styles.loginBox}>
+            <div className={styles.loginBox + ' loginbox'}>
                 <div>
+                    {/* 标题 */}
                     <div className={styles.title}>
                         <h1>XXX&nbsp;·&nbsp;通用后台管理系统</h1>
                         <p>Strive Everyday</p>
                     </div>
+                    {/* 输入框 */}
+                    <Space direction="vertical" size="large" style={{ display: 'flex' }}>
+                        <Input placeholder="用户名" />
+                        <Input.Password placeholder="密码" />
+                        <Button type="primary" block> 登录 </Button>
+                    </Space>
                 </div>
             </div>
         </div>
